5f899e3c792ea8dad248c4197fbc0f49372c4063
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 22:42:39 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,6 +2,6 @@ require 'pry'
 
 def my_all?(collection)
   new_collection = []
-  collection.each {|x|  new_collection << yield collection[x]}
+  collection.each {|x|  new_collection << yield(collection[x])}
   new_collection
 end
