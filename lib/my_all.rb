346219182ab1f643b6738846f5d185e57331dc56
Anusha Konatala <anu.konatala@gmail.com>
2018-06-04 22:48:19 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def my_all?(collection)
   i = 0
-  value
+  value = false
   while i < collection.length do
-    value << yield collection[i]
+    value = yield collection[i]
     i += 1
   end
-  
+  value
 end
